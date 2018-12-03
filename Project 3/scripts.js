3f0d0d01a725a598edea0ea48ae68d43f84fa183
Evan Falvey <efalvey19@mac-4510820dfyn.hanoverstudents.org>
2018-12-03 15:35:52 -0500

    Project 3

--- Project 3/scripts.js
+++ Project 3/scripts.js
@@ -1,23 +1,22 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<p id=""></p>
-
-<script>
-var x = 4;
-var y = 3;
-var z = 5;
-
-document.getElementById("demo").innerHTML = z;
-</script>
 
 function multiply() {
-    var value1 = getValue("value1")
-    var value2 = getValue("value2")
+    var value1 = getValue("value1");
+    var value2 = getValue("value2");
 
     writeResult(value1 * value2);
 
     document.getElementById("result").innerHTML = result;
-</body>
-</html>
+}
+function calculate(){
+  var side= getValue("side");
+  var volume= side * side * side;
+
+  writeResult(volume);
+}
+function writeResult(result){
+  document.getElementById("result").innerHTML = result;
+}
+
+function getValue(id) {
+  return parseFloat(document.getElementById(id).value);
+}
