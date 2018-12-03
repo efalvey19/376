52bb37a5281974dddfbdcaf2b22b5390cac00fd8
Evan Falvey <efalvey19@mac-4510820dfyn.hanoverstudents.org>
2018-12-03 13:46:34 -0500

    Project3

---
+++ Project 3/scripts.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id=""></p>
+
+<script>
+var x = 4;
+var y = 3;
+var z = 5;
+
+document.getElementById("demo").innerHTML = z;
+</script>
+
+function multiply() {
+    var value1 = getValue("value1")
+    var value2 = getValue("value2")
+
+    writeResult(value1 * value2);
+
+    document.getElementById("result").innerHTML = result;
+</body>
+</html>
