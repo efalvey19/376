1bd06d4b1d5d82f4cd4af819135ce952f80bfbcb
Evan Falvey <efalvey19@mac-4510820dfyn.hanoverstudents.org>
2018-12-04 07:50:45 -0500

    Project 3

--- Project 3/scripts.js
+++ Project 3/scripts.js
@@ -7,16 +7,31 @@ function multiply() {
 
     document.getElementById("result").innerHTML = result;
 }
-function calculate(){
+function calculateCubeVolume(){
   var side= getValue("side");
   var volume= side * side * side;
 
-  writeResult(volume);
+  writeResult(volume,"cubevolume");
 }
-function writeResult(result){
-  document.getElementById("result").innerHTML = result;
+function writeResult(value,elementId){
+  document.getElementById(elementId).innerHTML = value;
 }
 
 function getValue(id) {
   return parseFloat(document.getElementById(id).value);
+}
+function multiply() {
+    var value1 = getValue("value1");
+    var value2 = getValue("value2");
+    var value3 = getValue("value3")
+
+    writeResult(value1 * value2 * value3);
+
+    document.getElementById("result").innerHTML = result;
+}
+function calculatePrismVolume(){
+  var side= getValue("side");
+  var volume= side * side * side;
+
+  writeResult(volume,"prismvolume");
 }
