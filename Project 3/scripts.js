f4f1c0173cf7e7fc78d1aa969af3338e370f6c5d
Evan Falvey <efalvey19@mac-4510820dfyn.hanoverstudents.org>
2018-12-05 13:41:33 -0500

    Project Tres

--- Project 3/scripts.js
+++ Project 3/scripts.js
@@ -11,17 +11,16 @@ function calculateCubeVolume(){
   var side= getValue("side");
   var volume= side * side * side;
 
-  writeResult(volume,"cubevolume");
+  writeResult(volume,"cubeVolume");
 }
 function writeResult(value,elementId){
   document.getElementById(elementId).innerHTML = value;
 }
-
 function getValue(id) {
   return parseFloat(document.getElementById(id).value);
 }
 function multiply() {
-    var segment1 = getValue("value1");
+    var value1 = getValue("value1");
     var value2 = getValue("value2");
     var value3 = getValue("value3")
 
@@ -30,10 +29,16 @@ function multiply() {
     document.getElementById("result").innerHTML = result;
 }
 function calculatePrismVolume(){
-  var side= getValue("side");
-  var volume= side * side * side;
+  var segment= getValue("segment");
+  var volume= a * b * c
 
-  writeResult(volume,"prismvolume");
+  writeResult(volume,"prismVolume");
+}
+function writeResult(value,elementId){
+  document.getElementById(elementId).innerHTML = value;
+}
+function getValue(id) {
+  return parseFloat(document.getElementById(id).value);
 }
 function multiply() {
     var value1 = getValue("value1");
@@ -50,4 +55,10 @@ function calculatePyramidVolume(){
   var volume= (1/3) * b * h;
 
   writeResult(volume,"pyramidVolume");
+}
+function writeResult(value,elementId){
+  document.getElementById(elementId).innerHTML = value;
+}
+function getValue(id) {
+  return parseFloat(document.getElementById(id).value);
 }
